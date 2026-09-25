5b27ad7f302da59e04a3b20dccb57333d959c2dc
chrosslaw <Chrosslaw@gmail.com>
2026-09-25 08:02:46 -0500

    dm fix

--- ui/Header.tsx
+++ ui/Header.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 export default function Header() {
   return (
-    <header className="flex place-content-center p-2">
+    <header className="flex place-content-center ">
       <div className="flex flex-col md:flex-row md:p-4 max-w-6xl w-full rounded-md place-items-center ">
         <Image
           src={"/Logo.svg"}
@@ -11,10 +11,10 @@ export default function Header() {
           className="rounded-md "
         />
         <div className="flex flex-col place-content-start gap-2 w-full px-2">
-          <h1 className="sm:text-4xl md:text-5xl font-bold italic place-self-start px-2">
+          <h1 className="sm:text-4xl md:text-5xl font-bold italic place-self-start px-2 text-white">
             Welcome to Chris' Office.
           </h1>
-          <h2 className="flex sm:text-3xl lg:text-3xl font-bold italic self-end">
+          <h2 className="flex sm:text-3xl lg:text-3xl font-bold italic self-end text-white">
             Feel free to look around.
           </h2>
         </div>
